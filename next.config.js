ce3b1ed06032943d81aca4b0f883f59eb6c6139d
priyabajaria15 <priyabajaria15@gmail.com>
2026-06-08 15:18:45 -0400

    modified

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/box-tracker" : "",
+  assetPrefix: isProd ? "/box-tracker/" : "",
 };
 
 module.exports = nextConfig;
